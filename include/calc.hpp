82ff91fb160be018588a820978bcd04ba15931ae
Mikhail Aronov <aronov.michael@yandex.ru>
2026-03-13 23:33:50 +0300

    Add calc.hpp header file for basic arithmetic functions

---
+++ include/calc.hpp
@@ -0,0 +1,9 @@
+#ifndef CALC_HPP
+#define CALC_HPP
+
+double add(double a, double b);
+double sub(double a, double b);
+double mul(double a, double b);
+double div_safe(double a, double b);
+
+#endif
